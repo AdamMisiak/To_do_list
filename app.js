d802deda48d0e276bcc57e55e2e9ed8c21ca2d5b
AdamMisiak <adammisiak3@gmail.com>
2020-09-21 18:39:40 +0200

    Tasks priority colors added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const list = document.querySelector('.to-do-list');
 
 button.addEventListener('click', addToDoTask);
 list.addEventListener('click', changeToDoTask);
+list.addEventListener('click', changePriority);
 
 
 function addToDoTask(event){
@@ -14,6 +15,7 @@ function addToDoTask(event){
     {
         const todoBox = document.createElement('div');
         todoBox.classList.add('to-do-box');
+        todoBox.classList.add('low');
         list.appendChild(todoBox);
     
         const todoTask = document.createElement('li');
@@ -21,10 +23,16 @@ function addToDoTask(event){
         todoTask.classList.add('to-do-item');
         todoBox.appendChild(todoTask);
 
-        const todoMove = document.createElement('button');
-        todoMove.innerHTML = '<i class="fas fa-arrow-circle-right"></i>';
-        todoMove.classList.add('complete-button');
-        todoBox.appendChild(todoMove);
+        const todoPriority = document.createElement('button');
+        todoPriority.innerHTML = '<i class="fas fa-layer-group"></i>';
+        todoPriority.classList.add('priority-button');
+        todoBox.appendChild(todoPriority);
+    
+
+        const todoDone = document.createElement('button');
+        todoDone.innerHTML = '<i class="fas fa-check-circle"></i>';
+        todoDone.classList.add('complete-button');
+        todoBox.appendChild(todoDone);
     
         const todoDelete = document.createElement('button');
         todoDelete.innerHTML = '<i class="fas fa-minus-circle"></i>';
@@ -42,12 +50,32 @@ function changeToDoTask(event){
 
     if (clicked_item.classList[0] === 'delete-button'){
         const deleted_item = clicked_item.parentElement;
-        deleted_item.remove()  
+        deleted_item.classList.toggle('deleted');
+        deleted_item.addEventListener('transitionend', function (){
+            deleted_item.remove();
+        });
     }
 
     if (clicked_item.classList[0] === 'complete-button'){
         const completed_item = clicked_item.parentElement;
         completed_item.classList.toggle('completed')  
     }
-
 }
+
+
+function changePriority(event){
+    event.preventDefault();
+    var clicked_item = event.target;
+    console.log(clicked_item.parentElement.classList[1])
+    if (clicked_item.classList[0] === 'priority-button' && clicked_item.parentElement.classList[1] === 'low'){
+        clicked_item.parentElement.classList.remove('low');
+        clicked_item.parentElement.classList.add('medium');
+    } else if(clicked_item.classList[0] === 'priority-button' && clicked_item.parentElement.classList[1] === 'medium'){
+        clicked_item.parentElement.classList.remove('medium');
+        clicked_item.parentElement.classList.add('high');
+    } else if(clicked_item.classList[0] === 'priority-button' && clicked_item.parentElement.classList[1] === 'high'){
+        clicked_item.parentElement.classList.remove('high');
+        clicked_item.parentElement.classList.add('low');
+    }
+}
+
