a760ba99fafd05c39adcf7f1e6c2b203728be811
AdamMisiak <adammisiak3@gmail.com>
2020-09-21 16:09:53 +0200

    Completed style added

--- app.js
+++ app.js
@@ -5,39 +5,49 @@ const list = document.querySelector('.to-do-list');
 
 
 button.addEventListener('click', addToDoTask);
+list.addEventListener('click', changeToDoTask);
 
 
 function addToDoTask(event){
+    event.preventDefault();
     if (input.value != '')
     {
-        event.preventDefault()
-
-        const todoBox = document.createElement('div')
-        todoBox.classList.add('to-do-box')
-        list.appendChild(todoBox)
+        const todoBox = document.createElement('div');
+        todoBox.classList.add('to-do-box');
+        list.appendChild(todoBox);
     
-        const todoTask = document.createElement('li')
+        const todoTask = document.createElement('li');
         todoTask.innerText = input.value;
-        todoTask.classList.add('to-do-item')
-        todoBox.appendChild(todoTask)
+        todoTask.classList.add('to-do-item');
+        todoBox.appendChild(todoTask);
+
+        const todoMove = document.createElement('button');
+        todoMove.innerHTML = '<i class="fas fa-arrow-circle-right"></i>';
+        todoMove.classList.add('complete-button');
+        todoBox.appendChild(todoMove);
     
-        const todoDelete = document.createElement('button')
-        todoDelete.innerHTML = '<i class="fas fa-minus-circle"></i>'
-        todoDelete.classList.add('delete-button')
-        todoBox.appendChild(todoDelete)
-    
-        const todoMove = document.createElement('button')
-        todoMove.innerHTML = '<i class="fas fa-arrow-circle-right"></i>'
-        todoMove.classList.add('move-button')
-        todoBox.appendChild(todoMove)
+        const todoDelete = document.createElement('button');
+        todoDelete.innerHTML = '<i class="fas fa-minus-circle"></i>';
+        todoDelete.classList.add('delete-button');
+        todoBox.appendChild(todoDelete);
     
         input.value = '';
     }
+}
 
 
+function changeToDoTask(event){
+    event.preventDefault();
+    const clicked_item = event.target;
 
+    if (clicked_item.classList[0] === 'delete-button'){
+        const deleted_item = clicked_item.parentElement;
+        deleted_item.remove()  
+    }
 
-
-
+    if (clicked_item.classList[0] === 'complete-button'){
+        const completed_item = clicked_item.parentElement;
+        completed_item.classList.toggle('completed')  
+    }
 
 }
